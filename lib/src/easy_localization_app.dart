ccac62f9384b4274e62986e59800bdcd5a647357
Giles Correia Morton <gilescmsolutions@gmail.com>
2020-05-03 12:01:28 +0100

    Add optional preloader widget

--- lib/src/easy_localization_app.dart
+++ lib/src/easy_localization_app.dart
@@ -28,6 +28,7 @@ class EasyLocalization extends StatefulWidget {
   final assetLoader;
   final bool saveLocale;
   final Color preloaderColor;
+  final Widget preloaderWidget;
   EasyLocalization({
     Key key,
     @required this.child,
@@ -39,6 +40,7 @@ class EasyLocalization extends StatefulWidget {
     this.assetLoader = const RootBundleAssetLoader(),
     this.saveLocale = true,
     this.preloaderColor = Colors.white,
+    this.preloaderWidget
   })  : assert(child != null),
         assert(supportedLocales != null && supportedLocales.isNotEmpty),
         assert(path != null && path.isNotEmpty),
@@ -154,7 +156,7 @@ class _EasyLocalizationState extends State<EasyLocalization> {
             if (snapshot.connectionState == ConnectionState.waiting &&
                 !snapshot.hasData &&
                 !snapshot.hasError) {
-              returnWidget = EmptyPreloaderWidget();
+              returnWidget = widget.preloaderWidget ?? EmptyPreloaderWidget();
             } else if (snapshot.hasData && !snapshot.hasError) {
               returnWidget = _EasyLocalizationProvider(
                 widget,
